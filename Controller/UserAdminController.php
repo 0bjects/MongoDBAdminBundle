23244592472217e4707773c0f5cd427a3428c314
Mahmoud <mahmoud@objects.ws>
2013-06-23 14:57:32 +0200

    add main plugins and basic user admin

---
+++ Controller/UserAdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of UserAdminController
+ *
+ * @author Mahmoud
+ */
+
+namespace Objects\MongoDBAdminBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class UserAdminController extends Controller {
+
+}
+
+?>
---
+++ ObjectsMongoDBAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Objects\MongoDBAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ObjectsMongoDBAdminBundle extends Bundle
+{
+}
