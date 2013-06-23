e46e379dda1d6bec318288f8c590981e57d2bee6
Mahmoud <mahmoud@objects.ws>
2013-06-23 17:22:31 +0200

    add new actions to edit site configurations small design enhances fix form start does not print the parameters

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -4,6 +4,8 @@ namespace Objects\MongoDBAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Yaml\Dumper;
 
 /**
  * Admin controller.
@@ -52,6 +54,31 @@ class AdminController extends Controller {
             }
         }
         return $this->render('ObjectsMongoDBAdminBundle:Admin:sitePages.html.twig', array(
+                    'form' => $form->createView()
+        ));
+    }
+
+    public function siteConfigurationsAction() {
+        $container = $this->container;
+        $data = array();
+        $data['contact_email_to'] = $container->getParameter('contact_email_to');
+        $form = $this->createFormBuilder($data)
+                ->add('contact_email_to', 'email', array('constraints' => array(new NotBlank(), new Email())))
+                ->getForm();
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $configFilePath = __DIR__ . '/../../SiteBundle/Resources/config/config.yml';
+                $data = $form->getData();
+                $dumper = new Dumper();
+                file_put_contents($configFilePath, $dumper->dump(array('parameters' => $data), 3));
+                $request->getSession()->getFlashBag()->add('success', 'Saved Successfully');
+                exec(PHP_BINDIR . '/php-cli ' . __DIR__ . '/../../../../app/console cache:clear -e prod');
+                exec(PHP_BINDIR . '/php-cli ' . __DIR__ . '/../../../../app/console cache:warmup --no-debug -e prod');
+            }
+        }
+        return $this->render('ObjectsMongoDBAdminBundle:Admin:siteConfigurations.html.twig', array(
                     'form' => $form->createView()
         ));
     }
